95026c3dd8186d2f68f7c3cd34ce87d785609c01
hirahira <th11282@gmail.com>
2024-02-11 23:10:38 +0900

    ローディングスピナー対応

--- PartialViewSample/Controllers/HomeController.cs
+++ PartialViewSample/Controllers/HomeController.cs
@@ -1,18 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartialViewSample.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            return View(new SampleViewModel { Name = "太郎", Age = 20 });
+            // ユーザーのリストを作成（本来であればDB等からデータを取得する）
+            var userList = new List<UserModel>
+            {
+                new() { Name = "山田 太郎", Age = 30 },
+                new() { Name = "田中 次郎", Age = 20 },
+                new() { Name = "山田 花子", Age = 10 },
+            };
+
+            // 検索文字列が指定されている場合は条件に一致するユーザーのみを取得する
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                // 名前に検索文字列を含むユーザーを取得
+                userList = userList.Where(x => x.Name.Contains(searchString)).ToList();
+
+                // 確認のためわざと3秒間停止させる
+                await Task.Delay(3000);
+            }
+
+            return View(userList);
         }
     }
 
-    public class SampleViewModel
+    public class UserModel
     {
+        [Display(Name = "名前")]
         public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "年齢")]
         public int Age { get; set; }
     }
 }
