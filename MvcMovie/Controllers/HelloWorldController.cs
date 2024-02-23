2c4bbe251387adb8347d523595eb71ced8de112b
hirahira <th11282@gmail.com>
2024-02-23 20:55:51 +0900

    MvcMovieプロジェクトの追加

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // /HelloWorld にアクセスした時の処理
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // /HelloWorld/Welcome にアクセスした時の処理
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = $"こんにちは！{name}さん";
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
